f0d7812af1d12903cc5a9c133459e7eb3011ba54
Connork <connor.r.kelley18@gmail.com>
2022-06-04 12:38:35 -0400

    updating static files stuff

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ from django.urls import include, path
 from rest_framework import routers
 from blog import views
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'posts', views.PostViewSet)
@@ -31,4 +33,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
