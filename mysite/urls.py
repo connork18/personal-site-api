fe54400583f8c396b20cc8f2d419d9fd945f24e1
Connork <connor.r.kelley18@gmail.com>
2022-06-05 09:15:56 -0400

    removing url patterns for static

--- mysite/urls.py
+++ mysite/urls.py
@@ -34,4 +34,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-] + static(STATIC_URL, document_root=STATIC_ROOT)
+]
